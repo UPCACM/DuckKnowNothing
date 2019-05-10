b62fa9e044dedfc992a670465939ec4ec0b0c7cf
LucienShui <lucien@lucien.ink>
2019-05-10 19:43:27 +0800

    update vimrc, simplify fread

--- src/其他/输入输出优化.cpp
+++ src/其他/输入输出优化.cpp
@@ -1,26 +1,3 @@
-namespace Fread {
-    const int CACHE = 1 << 16;
-    char buf[CACHE];
-    size_t curl, curr;
-    inline char get() {
-        if (curl == curr) {
-            curl = 0, curr = fread(buf, 1, CACHE, stdin);
-            if (curr == 0) return EOF;
-        }
-        return buf[curl++];
-    }
-    template <typename type>
-    inline bool read(type &x, char c = '0') {
-        int flag = 1;
-        do {
-            c = get();
-            if (c == '-') flag = -1;
-        } while (c < '0' || c > '9');
-        do x = x * 10 + (c ^ '0'); while (c = get(), '0' <= c && c <= '9');
-        x *= flag;
-        return c != EOF;
-    }
-}
 namespace FastIO {
     const int SIZE = 1 << 16;
     char buf[SIZE], str[64];
